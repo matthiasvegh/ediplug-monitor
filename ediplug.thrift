b878cd754d2d3c141f24da10344130db706ba644
Matthias Vegh <matyas.vegh@gmail.com>
2016-08-08 00:28:40 +0200

    Print recent measurement values in frontend

--- ediplug.thrift
+++ ediplug.thrift
@@ -1,3 +1,18 @@
+struct Timepoint {
+1: double epoch
+}
+
+struct Consumption {
+1: double watts
+}
+
+struct Measurement {
+1: string plugName
+2: Timepoint timepoint
+3: Consumption consumption
+}
+
 service Ediplug {
 	double getCurrentConsumption()
+	list<Measurement> getConsumptionBetween(1: Timepoint begin_, 2: Timepoint end_)
 }
